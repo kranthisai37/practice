41e9b4cdd7e1a3c5a41a984eff127debcc897965
Kranthi sai <kranthisai20@gmail.com>
2026-01-29 15:51:27 +0530

    adamcheck.py

--- reverse.py
+++ reverse.py
@@ -18,3 +18,15 @@ print(isPalindrome((121)))
 print(reverse(123))
 print(isPalindrome((123)))
 
+def getPalindrome(start,end):
+    res=""
+    for i in range(1,end+1):
+        if isPalindrome(i):
+            res=res+str(i)+","
+    return res
+
+print(getPalindrome(1,1000))
+
+
+
+
