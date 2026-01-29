b6b0eaff18b81a9d1a7372e996e7233a6d8f6bef
Kranthi sai <155454009+kranthisai37@users.noreply.github.com>
2026-01-29 16:50:52 +0530

    Update 121 to 1219

--- reverse.py
+++ reverse.py
@@ -12,28 +12,18 @@ def reverse(num):
 def isPalindrome(num):
    return num==reverse(num)
 
-print(reverse(121))
+print(reverse(1219))
 print(isPalindrome((121)))
 
 print(reverse(123))
 print(isPalindrome((123)))
 
 def getPalindrome(start,end):
-<<<<<<< HEAD
-    res=""
-=======
-    res==""
->>>>>>> b1d4c5dd86c84d7ab8ea24d92ed50439aea406f1
+
     for i in range(1,end+1):
         if isPalindrome(i):
             res=res+str(i)+","
     return res
 
 print(getPalindrome(1,1000))
-<<<<<<< HEAD
 
-
-
-
-=======
->>>>>>> b1d4c5dd86c84d7ab8ea24d92ed50439aea406f1
